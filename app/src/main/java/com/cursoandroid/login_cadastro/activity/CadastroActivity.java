ff0060340c18458de89af17e5d4a178c6bab8b40
Rogerio JuR <rogeriojunior0101@gmail.com>
2020-06-01 11:50:17 -0300

    Commit alteração - 01/06

--- app/src/main/java/com/cursoandroid/login_cadastro/activity/CadastroActivity.java
+++ app/src/main/java/com/cursoandroid/login_cadastro/activity/CadastroActivity.java
@@ -17,6 +17,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class  CadastroActivity extends AppCompatActivity {
 
@@ -83,7 +86,20 @@ public class  CadastroActivity extends AppCompatActivity {
                     Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar usuario!",
                             Toast.LENGTH_SHORT).show();
                 }else {
-                    Toast.makeText(CadastroActivity.this, "Erro ao cadastrar usuario",
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    }catch ( FirebaseAuthWeakPasswordException e){
+                        excecao = "Digite uma senha mais forte!";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excecao = "Digite um email válido";
+                    }catch (FirebaseAuthUserCollisionException e){
+                        excecao = "Esta conta já foi cadastrada";
+                    }catch (Exception e){
+                        excecao = "Erro ao cadastrar usuário: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(CadastroActivity.this, excecao ,
                             Toast.LENGTH_SHORT).show();
                 }
             }
--- app/src/main/java/com/cursoandroid/login_cadastro/config/ConfiguracaoFirebase.java
+++ app/src/main/java/com/cursoandroid/login_cadastro/config/ConfiguracaoFirebase.java
@@ -14,4 +14,5 @@ public class ConfiguracaoFirebase {
         return autentitacao;
     }
 
+
 }
